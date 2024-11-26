dea4eb522047d41dbfa711eb47b691968006332f
Jpmasr3r <jpanjolimsoares@gmail.com>
2024-11-26 16:32:10 -0300

    commit

--- index.ts
+++ index.ts
@@ -4,9 +4,7 @@ import app from "./src/app.js";
 dotenv.config();
 
 try {
-	app.listen(process.env.PORT || 3333, () => {
-		console.log(`Server rodando em ${process.env.URL || ""}:${process.env.PORT || 3333}`);
-	});
+	app.listen(process.env.PORT || 3333);
 } catch (exception) {
 	const error = exception as Error;
 	console.log(`Error -> ${error.message}`);
